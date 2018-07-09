30297fc70073e8e117892b83b99cb0aab4b2ed64
Life4honor <choejy0329@gmail.com>
2018-07-09 20:58:32 +0900

    코드워즈 7kyu Get the Middle Character

--- java/src/july/ninth/GetTheMiddelCharacter/GetTheMiddleCharacter.java
+++ java/src/july/ninth/GetTheMiddelCharacter/GetTheMiddleCharacter.java
@@ -5,8 +5,8 @@ public class GetTheMiddleCharacter {
         //Code goes here!
         StringBuffer result = new StringBuffer();
         if(word.length()%2==0){
+            result.append(word.charAt(word.length()/2 - 1));
             result.append(word.charAt(word.length()/2));
-            result.append(word.charAt(word.length()/2 +1));
         }else{
             result.append(word.charAt(word.length()/2));
         }
