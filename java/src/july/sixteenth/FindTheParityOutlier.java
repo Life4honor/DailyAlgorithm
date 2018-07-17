7379248fe8ef5e33331833259d983052250f97b6
Life4honor <choejy0329@gmail.com>
2018-07-17 23:15:35 +0900

    코드워즈 6kyu Find The Parity Outlier

--- java/src/july/sixteenth/FindTheParityOutlier.java
+++ java/src/july/sixteenth/FindTheParityOutlier.java
@@ -1,7 +1,24 @@
 package july.sixteenth;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindTheParityOutlier {
     static int find(int[] integers) {
-        return 0;
+
+        List<Integer> oddNumber = new ArrayList<>();
+        List<Integer> evenNumber = new ArrayList<>();
+
+        for (int integer : integers) {
+            if(integer%2==0)
+                evenNumber.add(integer);
+            else
+                oddNumber.add(integer);
+        }
+
+        if(evenNumber.size()>oddNumber.size())
+            return oddNumber.get(0);
+        else
+            return evenNumber.get(0);
     }
 }
