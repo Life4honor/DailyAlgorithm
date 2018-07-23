d0e9bc1dfe2718f6fbb77101123fc7b923e336d1
Life4honor <choejy0329@gmail.com>
2018-07-23 22:56:06 +0900

    백준 BFS 숨바꼭질

--- java/src/july/twentyFirst/HideAndSeek.java
+++ java/src/july/twentyFirst/HideAndSeek.java
@@ -1,4 +1,7 @@
 package july.twentyFirst;
 
 public class HideAndSeek {
+    public static int seek(int subin, int young){
+        return 0;
+    }
 }
--- java/test/july/twentyFirst/HideAndSeekTest.java
+++ java/test/july/twentyFirst/HideAndSeekTest.java
@@ -1,6 +1,17 @@
 package july.twentyFirst;
 
 
+import org.junit.Test;
+
+import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
+
 public class HideAndSeekTest {
 
+    @Test
+    public void test() {
+        System.out.println("longest Fixed Tests");
+        assertEquals(4, HideAndSeek.seek(5, 17));
+    }
 }
