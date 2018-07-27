74a763ef44b8ab31bffdee5a7abae1e0aee0f2a4
Life4honor <choejy0329@gmail.com>
2018-07-27 20:13:23 +0900

    백준 1182 부분집합의 합

--- java/src/july/twentyForth/SumOfSubset.java
+++ java/src/july/twentyForth/SumOfSubset.java
@@ -1,7 +1,29 @@
 package july.twentyForth;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class SumOfSubset {
-    public static int sum(){
-        return 4;
+    public static int sum(int length, int expected, int[] inputs){
+        List<int[]> ints = Arrays.asList(inputs);
+        for (int[] anInt : ints) {
+            for (int i : anInt) {
+                System.out.println(i);
+            }
+
+        }
+
+//        boolean condition = ;
+        int result = 0;
+
+        System.out.println(ints.toString());
+        Collections.shuffle(ints);
+        System.out.println(ints.toString());
+//        if(condition)
+//            result++;
+
+        return result;
     }
 }
--- java/test/july/twentyForth/SumOfSubsetTest.java
+++ java/test/july/twentyForth/SumOfSubsetTest.java
@@ -7,7 +7,10 @@ import static org.junit.Assert.assertEquals;
 public class SumOfSubsetTest {
     @Test
     public void test(){
-        assertEquals(4, SumOfSubset.sum());
+        int length = 5;
+        int expected = 0;
+        int[] inputs = {-7, -3, -2, 5, 8};
+        assertEquals(1, SumOfSubset.sum(length, expected, inputs));
     }
 
 }
